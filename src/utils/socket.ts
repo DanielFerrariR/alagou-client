9a47da143144da2ba601cfe959bf3c4c3a6ab1ec
Daniel Ferrari <danielferrari@live.com>
2020-09-16 23:29:11 -0300

    feat(/): enabling sockets

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,27 +2,25 @@ import { useRef, useEffect } from 'react'
 import io from 'socket.io-client'
 import { API_ADDRESS } from '@env'
 import { FloodingsState, updateFloodings } from 'src/store/floodings'
-import { useDispatch, useSelector } from 'src/store'
-import { removeFavorite } from 'src/store/user'
+import { useDispatch } from 'src/store'
 
 const Socket: React.FC = () => {
-  // const userSession = useSelector((state) => state.user)
-  // const socket = useRef<SocketIOClient.Socket>(io(API_ADDRESS))
-  // const dispatch = useDispatch()
+  const socket = useRef<SocketIOClient.Socket>(io(API_ADDRESS))
+  const dispatch = useDispatch()
 
-  // useEffect(() => {
-  //   const asyncEffect = async () => {
-  //     socket.current.on('floodings', (floodings: FloodingsState) => {
-  //       dispatch(updateFloodings(floodings))
-  //     })
-  //   }
+  useEffect(() => {
+    const asyncEffect = async () => {
+      socket.current.on('floodings', (floodings: FloodingsState) => {
+        dispatch(updateFloodings(floodings))
+      })
+    }
 
-  //   asyncEffect()
+    asyncEffect()
 
-  //   return () => {
-  //     socket.current.disconnect()
-  //   }
-  // }, [])
+    return () => {
+      socket.current.disconnect()
+    }
+  }, [])
 
   return null
 }
