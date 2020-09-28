b4483089d1b51a5c2c880d7a381ee62399e5df3f
Daniel Ferrari <danielferrari@live.com>
2020-09-27 21:43:13 -0300

    feat(/): download using android download manager

--- src/components/screens/administration.tsx
+++ src/components/screens/administration.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { Container, MenuItem, Divider, Box } from 'src/components/atoms'
-import { Water } from 'src/images'
+import { Container, MenuItem, Divider } from 'src/components/atoms'
+import { Water, Excel } from 'src/images'
 import { BackHeader, MessageModal } from 'src/components/molecules'
 import { useWindowDimensions } from 'src/hooks'
 import DocumentPicker from 'react-native-document-picker'
@@ -11,6 +11,9 @@ import { useDispatch } from 'src/store'
 import { uploadFloodingsCSV } from 'src/store/floodings'
 import Geocoder from '@timwangdev/react-native-geocoder'
 import { toLatLon } from 'utm'
+import RNFetchBlob from 'rn-fetch-blob'
+import { API_ADDRESS } from '@env'
+import AsyncStorage from '@react-native-community/async-storage'
 
 type CSVData = string[][]
 
@@ -25,16 +28,18 @@ type DataStructure = {
 const Administration: React.FC = () => {
   const dimensions = useWindowDimensions()
   const dispatch = useDispatch()
-  const [loading, setLoading] = useState(false)
+  const [loadingImport, setLoadingImport] = useState(false)
+  const [loadingExport, setLoadingExport] = useState(false)
   const [successMessage, setSuccessMessage] = useState<string | string[]>('')
+  const [errorMessage, setErrorMessage] = useState<string | string[]>('')
 
   const pickCSV = async () => {
     try {
-      if (loading) {
+      if (loadingImport) {
         return
       }
 
-      setLoading(true)
+      setLoadingImport(true)
 
       const response = await DocumentPicker.pick({
         type: [DocumentPicker.types.csv]
@@ -99,17 +104,57 @@ const Administration: React.FC = () => {
 
           dispatch(await uploadFloodingsCSV(newFloodings))
 
-          setLoading(false)
+          setLoadingImport(false)
           setSuccessMessage('Alagamentos importados com sucesso!')
         }
       })
     } catch (error) {
-      setLoading(false)
+      setLoadingImport(false)
+
       if (DocumentPicker.isCancel(error)) {
         console.log(error)
       } else {
-        throw error
+        console.log(error)
       }
+    }
+  }
+
+  const exportCSV = async () => {
+    try {
+      if (loadingExport) {
+        return
+      }
+
+      setLoadingExport(true)
+
+      const userData = await AsyncStorage.getItem('@user')
+
+      if (!userData) {
+        return
+      }
+
+      const newUserData = JSON.parse(userData)
+
+      const headers = { Authorization: `Bearer ${newUserData.token}` }
+
+      const pathToWrite = `${RNFetchBlob.fs.dirs.DownloadDir}/data.csv`
+
+      await RNFetchBlob.config({
+        addAndroidDownloads: {
+          useDownloadManager: true,
+          notification: true,
+          mime: 'text/plain',
+          description: 'File downloaded by download manager.',
+          path: pathToWrite
+        }
+      }).fetch('GET', `${API_ADDRESS}/download-csv`, headers)
+
+      setSuccessMessage('CSV exportado para a pasta Downloads com sucesso!')
+
+      setLoadingExport(false)
+    } catch (error) {
+      setLoadingExport(false)
+      console.log(error)
     }
   }
 
@@ -117,19 +162,29 @@ const Administration: React.FC = () => {
     <>
       <BackHeader title="Administração" />
       <Container>
-        <Box>
-          <MenuItem
-            icon={() => <Water />}
-            onPress={() => pickCSV()}
-            title="Importar CSV com alagamentos"
-            style={{ maxWidth: '100%' }}
-            contentStyle={{ maxWidth: '100%' }}
-            contentStyleWithIcon={{
-              maxWidth: dimensions.width - 88
-            }}
-            loading={loading}
-          />
-        </Box>
+        <MenuItem
+          icon={() => <Water />}
+          onPress={() => pickCSV()}
+          title="Importar CSV com alagamentos"
+          style={{ maxWidth: '100%' }}
+          contentStyle={{ maxWidth: '100%' }}
+          contentStyleWithIcon={{
+            maxWidth: dimensions.width - 88
+          }}
+          loading={loadingImport}
+        />
+        <Divider />
+        <MenuItem
+          icon={() => <Excel />}
+          onPress={() => exportCSV()}
+          title="Exportar CSV com alagamentos"
+          style={{ maxWidth: '100%' }}
+          contentStyle={{ maxWidth: '100%' }}
+          contentStyleWithIcon={{
+            maxWidth: dimensions.width - 88
+          }}
+          loading={loadingExport}
+        />
         <Divider />
       </Container>
       <MessageModal
@@ -137,6 +192,7 @@ const Administration: React.FC = () => {
         setMessage={setSuccessMessage}
         success
       />
+      <MessageModal message={errorMessage} setMessage={setErrorMessage} error />
     </>
   )
 }
